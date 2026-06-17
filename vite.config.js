5f60230006eb6e84f7db6794fa870aa8bfa73d70
Imagonnafly <cedric.chrismario@gmail.com>
2026-06-17 12:36:54 +0530

    Updated vite config for local and github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/youtube-clone/', /* <-- ADD THIS LINE (Replace with your exact repo name) */
 })
